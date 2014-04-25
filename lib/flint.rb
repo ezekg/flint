9604f476583cf78435670a3b718c8c7a1c8e90f3
Ezekiel Gabrielse <ezekg@yahoo.com>
2014-04-25 09:03:19 -0500

    added aliases for `$gutter` arguments.

--- lib/flint.rb
+++ lib/flint.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('flint', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module Flint
-  VERSION = "1.0.5"
-  DATE = "2014-04-24"
+  VERSION = "1.0.6"
+  DATE = "2014-04-25"
 end
 
 # Custom functions
