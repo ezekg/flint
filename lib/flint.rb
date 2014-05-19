eeba27f53a67523174ffa9a9cdd74e4349ff8cc5
Ezekiel Gabrielse <ezekg@yahoo.com>
2014-05-19 11:00:33 -0500

    fixed issue with comma seperated parent sels. closes #7

--- lib/flint.rb
+++ lib/flint.rb
@@ -5,8 +5,8 @@ Compass::Frameworks.register('flint', :path => extension_path)
 #  Version is a number. If a version contains alphas, it will be created as a prerelease version
 #  Date is in the form of YYYY-MM-DD
 module Flint
-  VERSION = "1.3.0"
-  DATE = "2014-05-16"
+  VERSION = "1.3.1"
+  DATE = "2014-05-19"
 end
 
 # Custom functions
